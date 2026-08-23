bb2728670ecada388550b69ba97515cbc7be4bd8
Võ Văn Hoà <vovanhoa.bankgmail.com@Budda.local>
2026-08-23 23:56:14 +0700

    Update configs for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ocb-clone/',
+  base: '/T-i-ch-nh-daily/',
 })
