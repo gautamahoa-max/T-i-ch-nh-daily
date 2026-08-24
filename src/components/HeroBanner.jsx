f7a4ce0cefc9be4e1e1f0e45a64de807a907b145
Võ Văn Hoà <vovanhoa.bankgmail.com@Budda.local>
2026-08-24 12:38:07 +0700

    Stop banner from stretching past 1024px

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,12 +3,12 @@ import imgBanner from '../assets/images/banner.png';
 export default function HeroBanner() {
   return (
     <section className="w-full pt-[80px] bg-canvas overflow-hidden">
-      {/* Edge-to-edge full-width image container */}
-      <div className="w-full relative">
+      {/* Centered image container without stretching past native resolution */}
+      <div className="w-full relative bg-white flex justify-center">
         <img 
           src={imgBanner} 
           alt="OCB Credit Cards - Quẹt thẻ chi tiêu" 
-          className="w-full h-auto max-h-[70vh] object-cover object-center"
+          className="w-full max-w-[1024px] h-auto object-contain object-center"
           style={{ imageRendering: '-webkit-optimize-contrast' }}
         />
       </div>
