1441c8bc063937e64f8572ecb6fed139b6cd0ed8
Võ Văn Hoà <vovanhoa.bankgmail.com@Budda.local>
2026-08-24 13:14:07 +0700

    Update banner image and revert to full width

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,14 +1,14 @@
-import imgBanner from '../assets/images/banner.png';
+import imgBanner from '../assets/images/banner.jpg';
 
 export default function HeroBanner() {
   return (
     <section className="w-full pt-[80px] bg-canvas overflow-hidden">
-      {/* Centered image container without stretching past native resolution */}
-      <div className="w-full relative bg-white flex justify-center">
+      {/* Edge-to-edge full-width image container */}
+      <div className="w-full relative">
         <img 
           src={imgBanner} 
           alt="OCB Credit Cards - Quẹt thẻ chi tiêu" 
-          className="w-full max-w-[1024px] h-auto object-contain object-center"
+          className="w-full h-auto max-h-[70vh] object-cover object-center"
           style={{ imageRendering: '-webkit-optimize-contrast' }}
         />
       </div>
